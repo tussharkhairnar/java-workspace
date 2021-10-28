876d5b127c6b4eee7dd93954d07afb56805ad137
Tushar Khairnar <tushar.khairnar@itariumtech.com>
2021-10-28 13:09:57 +0530

    initial project

---
+++ springboot-first-web-application/src/test/java/com/practice/springbootfirstwebapplication/SpringbootFirstWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.springbootfirstwebapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootFirstWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
